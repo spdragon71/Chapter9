0580cad9a7730f97a649b8224649440f075b5f24
spdragon71 <spdragon@hanmail.net>
2026-03-12 18:23:37 +0900

    vercel test

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Chapter9",
+  //base: "/Chapter9", // Git-Hub Delpoy
 })
